fb1dd60eb596fa4b6b4bb4c364aafb5c0ca653de
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-06-03 20:12:27 -0300

    style: add styling to center app and format hello world text

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Hello World</Text>
     </View>
   );
 }
