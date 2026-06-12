7a11688982eda5fc1559127a391f4a3de84efefb
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-06-12 14:27:53 -0300

    chore: remove src/app/index.tsx boilerplate file

--- src/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Hello World</Text>
-    </View>
-  );
-}
